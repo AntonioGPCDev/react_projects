686a9897feffc488d50bab7e713cf96a11015d73
AntonioGPCDev <antoniogpcdev@gmail.com>
2025-01-08 13:29:27 +0100

    UI terminada y generación dinámica de cards

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 export function App(){
+
+    const users = [
+        { userName: 'midudev', name: 'Miguel Ángel Durán', isFollowing: true },
+        { userName: 'AntonioGPC_31', name: 'Antonio GPC', isFollowing: false }
+    ]
     
     return (
-        <div className='App'>
-            <TwitterFollowCard userName="midudev" initialIsFollowing={true}> 
-                Miguel Ángel Durán
-            </TwitterFollowCard> 
-            <TwitterFollowCard userName="AntonioGPC_31">
-                Antonio GPC
-            </TwitterFollowCard>
-        </div>
+        <section className='App'>
+            {
+                users.map(user =>  {
+                    const { userName, name, isFollowing } = user
+                    return (
+                        <TwitterFollowCard
+                            key={userName} 
+                            userName={userName} 
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
     )
 }
