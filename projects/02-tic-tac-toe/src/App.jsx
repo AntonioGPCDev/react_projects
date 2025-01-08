00ccaa171995cc5f5541fe8708f3eaf1e07c0b6f
AntonioGPCDev <antoniogpcdev@gmail.com>
2025-01-08 18:17:24 +0100

    tic-tac-toe con partida guardada

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -4,14 +4,21 @@ import { Square } from "./components/Square.jsx"
 import { TURNS } from "./constants.js"
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
 
 function App() {
 
   //Estado del tablero
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(()=> {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
   //Estado del turno
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState (()=> {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   //Estado del ganador
   const [winner, setWinner] = useState(null)
@@ -28,6 +35,12 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)// actualiza el estado de forma asíncrona
 
+    //Guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){ // si hay un ganador
       confetti()
@@ -41,6 +54,7 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
 
 
