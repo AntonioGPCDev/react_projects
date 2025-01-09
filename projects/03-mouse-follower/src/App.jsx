1cfd2edbca10116472a2b40b9ba93cf10f1d9230
AntonioGPCDev <antoniogpcdev@gmail.com>
2025-01-09 11:29:50 +0100

    project 3 acabado

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,8 +1,69 @@
+import {  useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const[enabled, setEnabled] = useState(false)
+  const[position, setPosition] = useState({x: 0, y: 0})
+
+  //pointer move
+  useEffect(() => {
+    console.log('effect', {enabled})
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //cleanup
+    // --> cuando el componente se desmonta
+    // --> cuando cambian las dependencias, antes de ejecutar
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  
+  //change body classname
+  useEffect(()=>{
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return() => { //cleanup method
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return(
+    <>
+    <div style={{
+      position:'absolute',
+      backgroundColor: '#09f',
+      borderRadius: '50%',
+      opacity: 0.7,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+    }}
+    />
+
+    <button onClick={() => setEnabled(!enabled)}>
+      {enabled? 'Desactivar': 'Activar' } seguir puntero
+    </button>
+    </>
+  )
+}
 
 function App() {
 
   return (
-    <h3> Proyecto 3 </h3>
+    <main>
+      <FollowMouse/>
+    </main>
+    
   )
 }
 
