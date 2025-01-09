e9246ffff9f32857fba6560781a8eb2c1b881909
AntonioGPCDev <antoniogpcdev@gmail.com>
2025-01-09 13:14:31 +0100

    p4 prueba tecnica con dos useEffect

--- projects/04-prueba-tecnica-react/src/App.jsx
+++ projects/04-prueba-tecnica-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react';
+import { use, useEffect, useState} from 'react';
 import './App.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 //const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/${firstWord}?'
@@ -6,28 +6,32 @@ const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
     const [ fact, setFact ] = useState()
+    const [ factError, setFactError] = useState()
     const [ imageUrl, setImageUrl ] = useState()
 
+    //para renderizar la pagina
     useEffect (() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(response => response.json())
             .then(data => {
-
                 const { fact } = data
                 setFact(fact)
+        })
+    }, []) //Dependencias: Array vacío para que se ejecute la primera vez
 
-                const threefirstWords = fact.split(' ',3).join(' ') // tres primeras palabras
+    useEffect(() => {
+        if(!fact) return
+
+        const threefirstWords = fact.split(' ',3).join(' ') // tres primeras palabras
                 console.log(threefirstWords)
 
                 fetch(`https://cataas.com/cat/says/${threefirstWords}?size=50&color=red&json=true`)
                  .then(response => response.json())
                  .then(response =>{
-                        const { url } = response
-                        setImageUrl(url)
+                    const { url } = response
+                    setImageUrl(url)
                  })
-        })
-    }, []) //Dependencias: Array vacío para que se ejecute la primera vez
-
+    }, [fact])
 
     return (
         <main>
