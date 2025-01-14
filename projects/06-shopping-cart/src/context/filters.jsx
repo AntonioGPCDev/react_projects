eb46024faf89724ad0ab13ee8a766bf09799f8f9
AntonioGPCDev <antoniogpcdev@gmail.com>
2025-01-14 11:40:58 +0100

    p6-shopping-cart parte del Ecommerce acabada

---
+++ projects/06-shopping-cart/src/context/filters.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState} from "react";
+
+//1. Crear el contexto. Este es el que tenemos que consumir
+// eslint-disable-next-line react-refresh/only-export-components
+export const FiltersContext = createContext()
+
+//2. Crear el provider para proveer el contexto. Este es el que nos provee de acceso al contexto
+export function FiltersProvider ({ children }){
+    const[ filters, setFilters] = useState ({
+        category:'all',
+        minPrice: 0
+    })
+    return (
+        <FiltersContext.Provider value = {{
+            filters,
+            setFilters
+        }}>
+            {children}
+        </FiltersContext.Provider>
+    )
+}
